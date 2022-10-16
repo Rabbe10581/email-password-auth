9554f645ad9f4584e76a500dd107cfc2a767f0c0
Rabbe <onikmahmudrabbe@gmail.com>
2022-10-17 02:44:06 +0600

    display error and reset form done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import { getAuth } from 'firebase/auth'
 import './App.css';
+import RegisterReactBootstrap from './components/RegisterReactBootstrap';
 import app from './firebase/firebase.init';
 
 const auth = getAuth(app);
 
+const handleRegister = (event) => {
+  event.preventDefault();
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+  console.log(email, password);
+}
+
+const handleEmailBlur = event => {
+  console.log(event.target.value);
+}
+const handlePasswordBlur = event => {
+  console.log(event.target.value);
+}
+
 function App() {
   return (
-    <div className="App">
-
+    <div className="">
+      <RegisterReactBootstrap></RegisterReactBootstrap>
     </div>
   );
 }
